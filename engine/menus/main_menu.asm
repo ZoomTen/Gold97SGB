098f0869b28861189588584585090820badffec5
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2020-12-08 12:12:14 +0700

    personalize version number

--- engine/menus/main_menu.asm
+++ engine/menus/main_menu.asm
@@ -32,6 +32,9 @@ MainMenu:
 	call MainMenu_PrintCurrentTimeAndDay
 	ld hl, .MenuHeader
 	call LoadMenuHeader
+	hlcoord 1, 11
+	ld de, MainMenu_PrintCurrentTimeAndDay.VersionNumberPrint;new
+	call PlaceString;new
 	call MainMenuJoypadLoop
 	call CloseWindow
 	jr c, .quit
@@ -188,8 +191,6 @@ MainMenu_PrintCurrentTimeAndDay:
 	ld b, a
 	decoord 1, 14
 	call PrintDayOfWeek
-	ld de, .VersionNumberPrint;new
-	call PlaceString;new
 	decoord 1, 16
 	ldh a, [hHours]
 	ld c, a
@@ -219,7 +220,7 @@ MainMenu_PrintCurrentTimeAndDay:
 	text_end
 	
 .VersionNumberPrint:
-	db "v3.20@"
+	db "v3.20-mod@"
 
 PrintDayOfWeek:
 	push de
